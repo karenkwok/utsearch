bf7dddb311b43792cdf2e3cf76b55ce52e527a1a
Karen Kwok <kkwok98@hotmail.com>
2021-03-05 22:26:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* jshint esversion: 6 */
+
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+/* code here */
+
+const PORT = process.env.PORT || 5000;
+
+http.createServer(app).listen(PORT, function (err) {
+  if (err) console.log(err);
+  else console.log("HTTP server on http://localhost:%s", PORT);
+});
