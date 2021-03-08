67513d9493a5e01fbbcf971ccd7d3e950b6cf899
Karen Kwok <kkwok98@hotmail.com>
2021-03-08 18:31:27 -0500

    added signup header, footer, headings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class SignupForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>UTSearCh</h1>
+        <h2>Sign In or Sign Up to continue</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<SignupForm></SignupForm>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
