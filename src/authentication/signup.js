9aa6cc75a12a1210c018ba29aa3c6b902e990332
Karen Kwok <kkwok98@hotmail.com>
2021-03-11 00:48:00 -0500

    moved signup files from index

---
+++ src/authentication/signup.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./signup.css";
+import "../index.css";
+
+class SignupForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>UTSearCh</h1>
+        <h2>Sign Up</h2>
+        <form id="signupform">
+          <p className="signupform-labels">Username</p>
+          <input type="text" className="signupform-fields" name="username" />
+
+          <p className="signupform-labels">Password</p>
+          <input
+            type="password"
+            className="signupform-fields"
+            name="password"
+          />
+
+          <p className="signupform-labels">Email</p>
+          <input type="text" className="signupform-fields" name="email" />
+
+          <input type="submit" id="createaccount-btn" value="Create Account" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignupForm;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-class SignupForm extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>UTSearCh</h1>
-        <h2>Sign Up</h2>
-        <form id="signupform">
-          <p className="signupform-labels">Username</p>
-          <input type="text" className="signupform-fields" name="username" />
-
-          <p className="signupform-labels">Password</p>
-          <input
-            type="password"
-            className="signupform-fields"
-            name="password"
-          />
-
-          <p className="signupform-labels">Email</p>
-          <input type="text" className="signupform-fields" name="email" />
-
-          <input type="submit" id="createaccount-btn" value="Create Account" />
-        </form>
-      </div>
-    );
-  }
-}
+import SignupForm from "./authentication/signup";
 
 ReactDOM.render(<SignupForm></SignupForm>, document.getElementById("root"));
