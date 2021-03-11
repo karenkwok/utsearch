65494a4ee51746436aa2bfced87932ab4477d319
Karen Kwok <kkwok98@hotmail.com>
2021-03-10 23:06:05 -0500

    made user schema

---
+++ models/users.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  username: String,
+  password: String,
+  email: String,
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", User);
