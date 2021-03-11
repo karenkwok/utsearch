4701df5c871268e6d605354a993c26ed6e3ad96f
Karen Kwok <kkwok98@hotmail.com>
2021-03-10 20:46:13 -0500

    made the signup ui

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 class SignupForm extends React.Component {
   render() {
     return (
       <div>
         <h1>UTSearCh</h1>
-        <h2>Sign In or Sign Up to continue</h2>
+        <h2>Sign Up</h2>
+        <form id="signupform">
+          <p className="signupform-labels">Username</p>
+          <input type="text" className="signupform-fields" name="username" />
+
+          <p className="signupform-labels">Password</p>
+          <input
+            type="password"
+            className="signupform-fields"
+            name="password"
+          />
+
+          <p className="signupform-labels">Email</p>
+          <input type="text" className="signupform-fields" name="email" />
+
+          <input type="submit" id="createaccount-btn" value="Create Account" />
+        </form>
       </div>
     );
   }
 }
 
 ReactDOM.render(<SignupForm></SignupForm>, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
