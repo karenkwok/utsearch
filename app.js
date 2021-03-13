55efe7d6576f2049259e13bca781577f098e4a5c
Karen Kwok <kkwok98@hotmail.com>
2021-03-12 21:59:06 -0500

    saved user info to mongodb
    installed and set up apollo graphql server

--- app.js
+++ app.js
@@ -3,12 +3,68 @@
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const User = require("./models/users");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type User { username: String, email: String }
+  type Query { users: [User] }
+  input CreateUserInput {
+    username: String
+    password: String
+    email: String
+  }
+  type Mutation {
+    CreateUser(input: CreateUserInput): User
+  }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: {
+    users: async () => {
+      const allUsers = await User.find();
+      return allUsers;
+    },
+  },
+  Mutation: {
+    CreateUser: async (_, { input }) => {
+      const newUser = User({ username: input.username, email: input.email });
+      await newUser.setPassword(input.password);
+      await newUser.save();
+      return newUser;
+    },
+  },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// Initialize the app
 const app = express();
 
+// The GraphQL endpoint
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
+// GraphiQL, a visual editor for queries
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 app.use(express.static("build"));
 
