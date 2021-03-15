b92fd1ceea1691cbd9d82df83fb87048b00eaab4
Karen Kwok <kkwok98@hotmail.com>
2021-03-15 19:10:16 -0400

    saved signup form info to mongodb

--- src/authentication/signup.js
+++ src/authentication/signup.js
@@ -1,34 +1,107 @@
 /* jshint esversion: 6 */
 
 import React from "react";
-import ReactDOM from "react-dom";
 import "./signup.css";
 import "../index.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { gql } from "@apollo/client";
+import { client } from "../index";
+
+// mutation query
+const CREATE_USER = gql`
+  mutation($input: CreateUserInput!) {
+    CreateUser(input: $input) {
+      username
+      email
+    }
+  }
+`;
 
 class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    // initial form state values
+    this.state = { username: "", password: "", email: "" };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // use apollo client to mutate with the variables from the form
+    client
+      .mutate({
+        variables: {
+          input: {
+            username: this.state.username,
+            password: this.state.password,
+            email: this.state.email,
+          },
+        },
+        // take the above variables into the mutation query above
+        mutation: CREATE_USER,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div id="signupform-wrapper">
         <h1>UTSearCh</h1>
         <h2>Sign Up</h2>
-        <form id="signupform">
+        <form id="signupform" onSubmit={this.handleSubmit}>
           <p className="signupform-labels">Username</p>
-          <input type="text" className="signupform-fields" name="username" />
+          <input
+            type="text"
+            className="signupform-fields"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
 
           <p className="signupform-labels">Password</p>
           <input
             type="password"
             className="signupform-fields"
             name="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
           />
 
           <p className="signupform-labels">Email</p>
-          <input type="text" className="signupform-fields" name="email" />
+          <input
+            type="text"
+            className="signupform-fields"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
 
           <input type="submit" id="createaccount-btn" value="Create Account" />
         </form>
-        <Link to="/signin" id="signupform-link">Already have an account? Sign In.</Link>
+        <Link to="/signin" id="signupform-link">
+          Already have an account? Sign In.
+        </Link>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,26 +5,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import SignupForm from "./authentication/signup";
 import SigninForm from "./authentication/signin";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Redirect } from "react-router-dom";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/signin" />
-          </Route>
-          <Route path="/signup">
-            <SignupForm></SignupForm>
-          </Route>
-          <Route path="/signin">
-            <SigninForm></SigninForm>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/signin" />
+            </Route>
+            <Route path="/signup">
+              <SignupForm></SignupForm>
+            </Route>
+            <Route path="/signin">
+              <SigninForm></SigninForm>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
