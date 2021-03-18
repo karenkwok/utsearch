c6b64cf89ec91f0cb60e7eff841455f983f2ef4d
Karen Kwok <kkwok98@hotmail.com>
2021-03-17 21:05:39 -0400

    implemented search ui

--- src/search/search.js
+++ src/search/search.js
@@ -3,6 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../index.css";
+import "./search.css";
 import { Link } from "react-router-dom";
 import Icon from '@material-ui/core/Icon';
 
@@ -13,13 +14,9 @@ class Search extends React.Component {
         <h1>UTSearCh</h1>
         <h2>Search for tags below!</h2>
         <form id="search-box">
-          <input type="text" className="search-field" name="Search" />
-
-          <Icon>search_icon</Icon>
-
-          <input type="submit" id="search-btn" value="Sign In" />
+          <input type="text" id="search-field" />
+          <button type="submit" id="search-btn"><Icon>search_icon</Icon></button>
         </form>
-        <Link to="/signup">Don't have an account? Sign Up.</Link>
       </div>
     );
   }
