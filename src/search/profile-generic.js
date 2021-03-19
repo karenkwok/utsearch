586e0ef41b3e80596a4d7c9b97d0a3c3dbc43a91
Karen Kwok <kkwok98@hotmail.com>
2021-03-19 17:23:50 -0400

    created generic profile ui

--- src/search/profile-generic.js
+++ src/search/profile-generic.js
@@ -4,31 +4,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../index.css";
 import "./profile-generic.css";
-import Icon from "@material-ui/core/Icon";
 
 class ProfileGeneric extends React.Component {
-    render() {
-      return (
-        <div id="profilegeneric-wrapper">
-          <div>
-            <img id="profilegeneric-picture" src={"../media/profilepic.png"} />
-          </div>
-          <h2>Username</h2>
-          <div id="profilegeneric-buttons">
-            <button>Friend</button>
-            <button>Call</button>
-            <button>Chat</button>
-            <button>Block</button>
-          </div>
-          <div id="profilegeneric-bio">
-            this is my biography.
-          </div>
-          <div id="profilegeneric-tags">
-            these are my super cool tags.
-          </div>
+  render() {
+    return (
+      <div id="profilegeneric-wrapper">
+        <div>
+          <img id="profilegeneric-picture" src={"../media/profilepic.png"} />
         </div>
-      );
-    }
+        <h2>Username</h2>
+        <div id="profilegeneric-buttons">
+          <button className="profilegeneric-button">+Friend</button>
+          <button className="profilegeneric-button">Call</button>
+          <button className="profilegeneric-button">Chat</button>
+          <button className="profilegeneric-button">Block</button>
+        </div>
+        <div id="profilegeneric-bio">this is my biography.</div>
+        <div id="profilegeneric-tags">
+          <div className="profilegeneric-tag">these</div>
+          <div className="profilegeneric-tag">are</div>
+          <div className="profilegeneric-tag">my</div>
+          <div className="profilegeneric-tag">super</div>
+          <div className="profilegeneric-tag">awesome</div>
+          <div className="profilegeneric-tag">tags.</div>
+        </div>
+      </div>
+    );
   }
-  
-  export default ProfileGeneric;
+}
+
+export default ProfileGeneric;
