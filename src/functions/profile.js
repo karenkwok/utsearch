f6f218ada85627d95bedb248148a79dd550b7e3c
Karen Kwok <kkwok98@hotmail.com>
2021-03-20 15:40:44 -0400

    converted class component to function component

--- src/functions/profile.js
+++ src/functions/profile.js
@@ -3,47 +3,58 @@
 import React from "react";
 import "./profile.css";
 import "../index.css";
-import Icon from '@material-ui/core/Icon';
-
+import Icon from "@material-ui/core/Icon";
 import Tabs from "../components/tabs";
 
-class Profile extends React.Component {
-  render() {
-    //Tabs functionality from https://www.digitalocean.com/community/tutorials/react-tabs-component
-
-    return (
-      <div id="profile-wrapper">
-        <div id="profile-picture-wrapper">
-          <img id="profile-picture" src={"../media/default.png"} alt="Profile Picture"/>
-          <button id="img-btn"><Icon>edit_icon</Icon></button>
-          
-        </div>
-
-        <h1 id="profile-name">The Username</h1>
-        <h3>FirstName LastName</h3>
-
-        <div id="button-wrapper">
-          <Tabs>
-            <div className="profile-buttons" label="My Info">
-              <div>
-                <textarea className="profile-input" placeholder="Your Bio" name="Text1" rows="5"></textarea>
-              </div>
-              <div>
-                <textarea className="profile-input" placeholder="Your Tags" name="Text1" rows="5"></textarea>
-              </div>
-              <button> Save</button>
-            </div>
-            <div className="profile-buttons" label="Friends">
-              Your friends list is empty :(
-            </div>
-            <div className="profile-buttons" label="Blocked">
-              Your blocked list is empty :)
-            </div>
-          </Tabs>
-        </div>
+function Profile() {
+  //Tabs functionality from https://www.digitalocean.com/community/tutorials/react-tabs-component
+  return (
+    <div id="profile-wrapper">
+      <div id="profile-picture-wrapper">
+        <img
+          id="profile-picture"
+          src={"../media/default.png"}
+          alt="Profile Picture"
+        />
+        <button id="img-btn">
+          <Icon>edit_icon</Icon>
+        </button>
       </div>
-    );
-  }
+
+      <h1 id="profile-name">The Username</h1>
+      <h3>FirstName LastName</h3>
+
+      <div id="button-wrapper">
+        <Tabs>
+          <div className="profile-buttons" label="My Info">
+            <div>
+              <textarea
+                className="profile-input"
+                placeholder="Your Bio"
+                name="Text1"
+                rows="5"
+              ></textarea>
+            </div>
+            <div>
+              <textarea
+                className="profile-input"
+                placeholder="Your Tags"
+                name="Text1"
+                rows="5"
+              ></textarea>
+            </div>
+            <button> Save</button>
+          </div>
+          <div className="profile-buttons" label="Friends">
+            Your friends list is empty :(
+          </div>
+          <div className="profile-buttons" label="Blocked">
+            Your blocked list is empty :)
+          </div>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
