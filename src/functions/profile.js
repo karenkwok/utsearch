7c663146bf9ff017f33d328a443ae0b6070071bc
Karen Kwok <kkwok98@hotmail.com>
2021-03-20 18:22:53 -0400

    displayed tags on profile when submit button hit

--- src/functions/profile.js
+++ src/functions/profile.js
@@ -1,22 +1,52 @@
 /* jshint esversion: 6 */
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./profile.css";
 import "../index.css";
 import Icon from "@material-ui/core/Icon";
 import Tabs from "../components/tabs";
-import { Redirect, useParams } from "react-router";
+import { useParams } from "react-router";
 import { Context } from "../Store";
+import { client } from "..";
+import { gql } from "@apollo/client";
+
+const CREATE_TAG = gql`
+  mutation($input: String!) {
+    CreateTag(input: $input)
+  }
+`;
 
 function Profile() {
-  const { username } = useParams();
   const [state, dispatch] = useContext(Context);
+  const [tag, setTag] = useState("");
+  const [tags, setTags] = useState(state.user.tags);
+  const { username } = useParams();
 
   if (username !== state.user.username) {
     return <p>"You can't do that."</p>;
   }
 
-  const tags = state.user.tags;
+  const handleTagChange = function (event) {
+    setTag(event.target.value);
+  };
+
+  const handleTagSave = function () {
+    client
+      .mutate({
+        variables: {
+          input: tag,
+        },
+        mutation: CREATE_TAG,
+      })
+      .then((result) => {
+        setTags(result.data.CreateTag);
+        setTag("");
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   //Tabs functionality from https://www.digitalocean.com/community/tutorials/react-tabs-component
   return (
@@ -55,9 +85,11 @@ function Profile() {
                 placeholder="Your Tags"
                 name="Text1"
                 rows="5"
+                onChange={handleTagChange}
+                value={tag}
               ></textarea>
             </div>
-            <button> Save</button>
+            <button onClick={handleTagSave}>Save</button>
           </div>
           <div className="profile-buttons" label="Friends">
             Your friends list is empty :(
