e87dc854f1bb74d62e249c560602bd8003e86f27
Karen Kwok <kkwok98@hotmail.com>
2021-04-07 18:20:51 -0400

    saved location to database

--- src/components/people/maps/maps.js
+++ src/components/people/maps/maps.js
@@ -1,13 +1,29 @@
 /* jshint esversion: 6 */
 
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Context } from "../../../Store";
+import { client } from "../../..";
 import "./maps.css";
 import "../../../index.css";
 import GoogleMapReact from "google-map-react";
+import { gql } from "@apollo/client";
 const MyLocation = ({ text }) => <div>{text}</div>;
 
+const EDIT_LOCATION = gql`
+  mutation($lat: Float!, $long: Float!) {
+    CreateLocation(lat: $lat, long: $long) {
+      username
+      myLocation {
+        lat
+        long
+      }
+    }
+  }
+`;
+
 function Maps() {
-  // set default location to UTSC (if the user's browser doesnt support HTML5 or if they decide to turn location off)
+  const [state, dispatch] = useContext(Context);
+  // set default location to UTSC (if the user's browser doesnt support geolocation or if they decide to turn location off)
   const [lat, setLat] = useState(43.783195941658995);
   const [long, setLong] = useState(-79.18734841588014);
 
@@ -16,18 +32,33 @@ function Maps() {
     const location = (position) => {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
-    }
+      client
+        .mutate({ variables: { lat: lat, long: long }, mutation: EDIT_LOCATION })
+        .then((result) => {
+          dispatch({
+            type: "EDIT_LOCATION",
+            payload: result.data.CreateLocation.myLocation,
+          });
+          console.log(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
     navigator.geolocation.getCurrentPosition(location);
   }
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact bootstrapURLKeys={{ key: "AIzaSyA-Srzn7JIgECii-KKAKSzhf-aixVZngpI" }}
-          defaultCenter={{
-            lat: lat,
-            lng: long
-          }}
-          defaultZoom={11}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyA-Srzn7JIgECii-KKAKSzhf-aixVZngpI" }}
+        defaultCenter={{
+          lat: lat,
+          lng: long,
+        }}
+        defaultZoom={16}
+      >
         <MyLocation lat={lat} lng={long} text="&#128514;" />
       </GoogleMapReact>
     </div>
