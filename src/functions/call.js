b93092b11547a5b410556f8d66c8736743583f3f
Jordan Feng <jordan.feng@hotmail.com>
2021-04-07 19:20:51 -0400

    Hopefully this works?

--- src/functions/call.js
+++ src/functions/call.js
@@ -9,11 +9,35 @@ import CallComponent from "../components/call-component";
 
 
 class Call extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {call: false};
+  }
+
+  startMeeting = ev => {
+    this.setState({ call: !this.state.call });
+  }
+
   render() {
     return (
 
-      <div id="video-chat-wrapper">
-      <CallComponent />
+      <div id="call-wrapper">
+
+        {!this.state.call ?
+          <>
+            <h1>Meet A Student!</h1>
+            <h3>Welcome to Random Chat!</h3>
+            <p>Random Chat is a great way to meet new friends from UTSC!
+            </p>
+            <p>
+              We show other anonymous students currently online, where you can send and/or accept invites to a video call!
+            </p>
+
+            <button id="meet-student-button" onClick={this.startMeeting}>Meet A Student!</button>
+          </>
+          :
+          <CallComponent/>
+        }
       </div>
     );
   }
--- src/functions/videoChat.js
+++ src/functions/videoChat.js
@@ -9,11 +9,35 @@ import VideoChatComponent from "../components/video-chat-component";
 
 
 class VideoChat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {video: false};
+  }
+
+  startMeeting = ev => {
+    this.setState({ video: !this.state.video });
+  }
+
   render() {
     return (
 
       <div id="video-chat-wrapper">
-      <VideoChatComponent />
+
+        {!this.state.video ?
+          <>
+            <h1>Meet A Student!</h1>
+            <h3>Welcome to Random Chat!</h3>
+            <p>Random Chat is a great way to meet new friends from UTSC!
+            </p>
+            <p>
+              We show other anonymous students currently online, where you can send and/or accept invites to a video call!
+            </p>
+
+            <button id="meet-student-button" onClick={this.startMeeting}>Meet A Student!</button>
+          </>
+          :
+          <VideoChatComponent />
+        }
       </div>
     );
   }
